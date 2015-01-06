f33dbda888f749e68828fcab2ff9313bd7d07d14
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-01-06 09:11:57 -0200

    Definindo JSON automático para rota /form

--- web/src/PensandooDireito/FormsBundle/Controller/DefaultController.php
+++ web/src/PensandooDireito/FormsBundle/Controller/DefaultController.php
@@ -2,12 +2,19 @@
 
 namespace PensandooDireito\FormsBundle\Controller;
 
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     public function indexAction($name)
     {
-        return $this->render('PensandooDireitoFormsBundle:Default:index.html.twig', array('name' => $name));
+
+        $view = $this->view(array('name' => $name), 200)
+//            ->setTemplate("PensandooDireitoFormsBundle:Default:index.html.twig")
+//            ->setFormat('json')
+        ;
+
+        return $this->handleView($view);
     }
 }
